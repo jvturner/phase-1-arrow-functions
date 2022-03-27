8b486d915821f926549ebc4ddedd05518e8d45f1
Jamond Turner <jamondturner@yahoo.com>
2022-03-27 19:27:12 -0400

    Completed Assignment

---
+++ index.js
@@ -0,0 +1,42 @@
+// const divide = (num1, num2) => num1 / num2;
+// divide (2000,100);
+
+let a = 2000;
+let b = 100;
+
+
+
+function divide (a,b){
+    return a / b
+};
+console.log(a / b);
+divide();
+
+() => (a / b)
+// ///////////////////////////////////////////////////////////////////////////////////////////
+
+// const square = (num1) => (num1 * num1);
+// square (2);
+
+let x = 2;
+
+function square (x){
+    return (x * x)
+};
+console.log(x * x);
+square();
+
+() => {x * x};
+// ///////////////////////////////////////////////////////////////////////////////////////
+
+let c = 3;
+let d = 4;
+
+function add (c,d){
+    return c + d
+};
+console.log(c + d);
+add();
+
+() => (c + d);
+
